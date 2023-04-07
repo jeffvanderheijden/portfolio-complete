6186dca329a6a6bc6c5a97ed496ac41166ad2005
Jeff van der Heijden <jheijden@glr.nl>
2023-04-07 22:20:29 +0200

    Added text animation to homepage header and fixed CustomCursor bug in iframe

--- src/components/Headers/Homepage/Header.jsx
+++ src/components/Headers/Homepage/Header.jsx
@@ -1,24 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import { MouseContext } from '../../../helpers/context/mouseContext'
+import AnimatedText from "./AnimatedText"
+import { motion } from "framer-motion"
 import "./Header.scss"
 
 const Header = () => {
+    const [replay] = useState(true)
+
+    const placeholderText = [
+        { type: "heading2", text: "I turn data into" },
+        { type: "heading1", text: "experiences", color: "#eba10e" },
+        { type: "heading2", text: "that people care about" }
+    ]
+
+    const container = {
+        visible: {
+            transition: {
+                staggerChildren: 0.025
+            }
+        }
+    }
+
     return (
         <div className="homeHeader">
             <MouseContext.Consumer>
                 {({ cursorChangeHandler }) => (
-                    <iframe 
-                        src="/static/threejs.html" 
-                        title="Homepage header" 
+                    <iframe
+                        src="/static/threejs.html"
+                        title="Homepage header"
                         onMouseEnter={() => cursorChangeHandler("hidden")}
-                        onMouseLeave={() => cursorChangeHandler("")} 
+                        onMouseLeave={() => cursorChangeHandler("")}
                     />
                 )}
             </MouseContext.Consumer>
-            <div className="container mx-auto relative pointer-events-none">
-                <h1>I turn data into experiences</h1>
-                <h1>that people care about</h1>
-            </div>
+            <motion.div
+                className="w-full"
+                initial="hidden"
+                animate={replay ? "visible" : "hidden"}
+                variants={container}
+            >
+                <div className="container mx-auto relative pointer-events-none">
+                    {placeholderText.map((item, index) => {
+                        return <AnimatedText {...item} key={index} />
+                    })}
+                </div>
+            </motion.div>
         </div>
     )
 }

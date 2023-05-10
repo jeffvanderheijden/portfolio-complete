6a21891be4eb443017d10d88abae9e333dfa5029
Jeff van der Heijden <jheijden@MacBook-Pro-van-Jeff.local>
2023-05-10 11:30:02 +0200

    Responsive fixes + back in nav on pages other than home

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { useLocation } from '@reach/router'
 import { Link } from "gatsby"
 import { motion } from "framer-motion"
 import { MouseContext } from "../../helpers/context/mouseContext"
@@ -9,6 +10,9 @@ import Foldout from "./Foldout"
 
 const Navigation = () => {
   const [navOpen, setNavOpen] = useState(false)
+  const location = useLocation()
+
+  console.log(location.pathname)
 
   return (
     <>
@@ -48,31 +52,45 @@ const Navigation = () => {
                     <LogoInner />
                   </Link>
                 </div>
-                <div
-                  className="hamburger"
-                  onClick={() => { 
-                    setNavOpen(!navOpen); 
-                    navOpen ? cursorChangeHandler("hovered") : cursorChangeHandler("hovered white")
-                  }}
-                  onMouseEnter={() => {
-                    navOpen ? cursorChangeHandler("hovered white") : cursorChangeHandler("hovered")
-                  }}
-                  onMouseLeave={() => {
-                    navOpen ? cursorChangeHandler("white") : cursorChangeHandler("")
-                  }}
-                >
-                  <svg className={`ham hamRotate ham8 ${navOpen ? "active" : ""}`} viewBox="0 0 100 100" width="40">
-                    <path
-                      className="line top"
-                      d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
-                    <path
-                      className="line middle"
-                      d="m 30,50 h 40" />
-                    <path
-                      className="line bottom"
-                      d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
-                  </svg>
-                </div>
+                {location.pathname !== '/' ? (
+                  <Link
+                    to="/"
+                    onMouseEnter={() => {
+                      navOpen ? cursorChangeHandler("hovered white") : cursorChangeHandler("hovered")
+                    }}
+                    onMouseLeave={() => {
+                      navOpen ? cursorChangeHandler("white") : cursorChangeHandler("")
+                    }}
+                  >
+                    <div className="text-white">back</div>
+                  </Link>
+                ) : (
+                  <div
+                    className="hamburger"
+                    onClick={() => { 
+                      setNavOpen(!navOpen); 
+                      navOpen ? cursorChangeHandler("hovered") : cursorChangeHandler("hovered white")
+                    }}
+                    onMouseEnter={() => {
+                      navOpen ? cursorChangeHandler("hovered white") : cursorChangeHandler("hovered")
+                    }}
+                    onMouseLeave={() => {
+                      navOpen ? cursorChangeHandler("white") : cursorChangeHandler("")
+                    }}
+                  >
+                    <svg className={`ham hamRotate ham8 ${navOpen ? "active" : ""}`} viewBox="0 0 100 100" width="40">
+                      <path
+                        className="line top"
+                        d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
+                      <path
+                        className="line middle"
+                        d="m 30,50 h 40" />
+                      <path
+                        className="line bottom"
+                        d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
+                    </svg>
+                  </div>
+                )}
               </div>
             </div>
           )}
